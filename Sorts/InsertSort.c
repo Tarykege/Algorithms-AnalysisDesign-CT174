2bdc92f6fd2e9bffbd423c95bb5ee2692b9c36c7
Tarykege <xoiiwkingg@gmail.com>
2021-12-27 14:34:16 +0700

    upload insertsort

--- Sorts/InsertSort.c
+++ Sorts/InsertSort.c
@@ -1,95 +1,20 @@
 #include<stdio.h>
-#define Max_Vertices 20
-#define Max_length 20
-typedef struct {
-    int A[Max_Vertices][Max_Vertices];
-    int n;
-}Graph;
-typedef struct {
-    int data[Max_length];
-    int size;
-}List;
-// Ham khoi tao do thi
-void init_Graph(Graph *G, int n){
-    int i,j;
-    G->n=n;
-    for(i=1; i<=n ;i++){
-        for(j=1; j<=n ;j++){
-            G->A[i][j]=0;
-        }
-    }
-}
-// Ham them cung vao do thi vo huong 2->3 & 3->2 = 1
-void add_edge(Graph *G, int x, int y){
-    G->A[x][y]=1;
-    G->A[y][x]=1;
-}
-// Ham kiem tra lang gieng
-int adjacent(Graph *G, int x, int y){
-    return (G->A[x][y]!=0);
-}
-// Ham tinh bac cua dinh
-int degree(Graph *G, int x){
-    int i,deg=0;
-    for(i=1; i<=G->n; i++){
-        if(G->A[i][x]==1) deg++; //adjacent(G,i,x)
-    }
-    return deg;
-}
-// khoi tao list de luu dinh lang gieng
-void make_nullList(List *L){
-    L->size=0;
-}
-// them dinh lang gieng x vao list
-void push_back(List *L, int x){
-    L->data[L->size]=x;
-    L->size++;
-}
-// tra ve 1 list cac dinh lang gieng cua dinh x
-List neightbors(Graph *G, int x){
-    int i;
-    List L;
-    make_nullList(&L);
-    for(i=1; i<=G->n; i++){
-        if(G->A[i][x]==1) push_back(&L,i); //adjacent(G,i,x)
-    }
-    return L;
-}
-// lay dinh tai vi tri i
-int element_at(List *L, int i){
-    return L->data[i-1];
+void insert_sort(int a[], int n){
+	int i,j,key;
+	// chay tu phan tu thu 2 vi phan tu thu 1 coi nhu da duoc sap xep
+	for(int i=1; i<n; i++){
+		key=a[i];
+		j=i-1;
+		// xet gia tri phia truoc cua key co lon hon key khong, neu co thi lui, den khi gap gia tri nho hon vao gan gia tri vao phia sau
+		while(a[j]>key && j>=0){
+			a[j+1]=a[j];
+			j--;
+		}
+		a[j+1]=key;
+	}
 }
 int main(){
-    Graph G;
-    FILE *file = freopen("input2.txt","r",stdin);
-    int n,m,u,v,i,j;
-    scanf("%d%d",&n,&m);
-    init_Graph(&G,n);
-
-    // them cac cung vao do thi G
-    for(i=1; i<=m; i++){
-        scanf("%d%d",&u,&v);
-        add_edge(&G,u,v);
-    }
-
-    // in don do thi duoi dang ma tran dinh dinh
-    for(i=1; i<=G.n; i++){
-        for(j=1; j<=G.n; j++){
-            printf("%d ",G.A[i][j]);
-        }
-        printf("\n");
-    }
-
-    // in cac dinh lang giang cua tung dinh trong do thi
-    for(i=1; i<=G.n; i++){
-        List L = neightbors(&G, i);
-        printf("Neighbor(%d):",i);
-        for(j=1; j<=L.size; j++){
-            printf(" %d",element_at(&L,j));
-        }
-        printf("\n");
-    }
-
-    fclose(file);
+	int a[] = {6,4,5,1,67,6};
+	insert_sort(a,6);
+	return 0;
 }
-
---
+++ Sorts/README.md
@@ -0,0 +1,34 @@
+# Sorts
+
+## day 1 (27/12/2021): INSERT SORT
+
+### Ý tưởng
+```
+Bắt chước cách sắp xếp quân bài của những người chơi bài
+```
+
+### Nguyên lý
+```
+- Chạy từ đầu đến cuối mảng
+- Tại vòng lặp i, coi nhu dãy từ [0,i-1] đã được sắp xếp
+- Chèn phần từ a[i] vào vị trí thích hợp (sao cho phần tử đúng trạng thái sắp xếp khi thêm vào)
+- Sau vòng lặp thứ i thì dãy [0,i] đã được sắp xếp.  
+```
+### Độ phức tạp
+Độ phức tạp về thời gian: O(n*2)
+Không gian phụ trợ: O(1)
+
+## day 1.2 (27/12/2021): SELECTION SORT
+
+### Ý tưởng
+```
+
+```
+
+### Nguyên lý
+```
+ 
+```
+### Độ phức tạp
+Độ phức tạp về thời gian: O()
+Không gian phụ trợ: O()
